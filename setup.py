8f315bb2eae3cf811cd48ecf9da16c840c981c8a
Eric Reynolds <eric.remo.reynolds@gmail.com>
2016-08-05 11:15:36 +0300

    Tweaks

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from setuptools import setup
 
 setup(
     name="fix-srt",
+    version="1.0.0",
+    author="Eric Reynolds",
+    author_email="eric.remo.reynolds@gmail.com",
+    description="A tiny Python package to fix SRT subtitle files when out of sync with audio",
+    license="MIT",
+    keywords="subtitle subtitles srt sync audio synchronize",
+    url="https://github.com/ericremoreynolds/fix-srt",
     scripts=[
         "fix-srt.py"
     ]
